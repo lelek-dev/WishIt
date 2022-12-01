ce5d41b8492ee586800bde01de7a69e70d77f408
Jonathan  Lelek <jonathanlelek@aol.de>
2022-12-01 20:33:50 +0100

    simple fixes

--- wishit/settings.py
+++ wishit/settings.py
@@ -77,8 +77,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'social_django.context_processors.backends',
-                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -135,11 +133,8 @@ SOCIAL_AUTH_AUTH0_SCOPE=[
     'email'
 ]
 AUTHENTICATION_BACKENDS={
-    'social_core.backends.open_id.OpenIdAuth',
-    'social_core.backends.google.GoogleOAuth2',
-    'social_core.backends.google.GoogleOAuth',
-    'social_core.backends.twitter.TwitterOAuth',
-    'django.contrib.auth.backends.ModelBackend',
+    'social_core.backends.auth0.Auth0OAuth2',
+    'django.contrib.auth.backends.ModelBackend'
 }
 SOCIAL_AUTH_URL_NAMESPACE = 'externalAuth'
 # Internationalization
